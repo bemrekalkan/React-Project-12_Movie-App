10ab0861cf101b6619b0174ef294f98ce65836cb
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-19 21:00:28 +0300

    register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 const Navbar = () => {
-  const currentUser = {
-    displayName: "Felix Franko",
-  };
+  const navigate = useNavigate();
+  // const currentUser = {
+  //   displayName: "felix franko",
+  // };
+  const currentUser = false;
   return (
     <div>
       <nav className="navbar navbar-expand-lg">
@@ -12,7 +13,7 @@ const Navbar = () => {
           <Link to={"/"} className="navbar-brand text-white">
             <h4>React Movie App</h4>
           </Link>
-          <div className="d-flex text-white align-items-center ">
+          <div className="d-flex text-white align-items-center">
             {currentUser ? (
               <>
                 <h5 className="mb-0 text-capitalize">
@@ -20,7 +21,22 @@ const Navbar = () => {
                 </h5>
                 <button className="ms-2 btn btn-outline-light">Logout</button>
               </>
-            ) : null}
+            ) : (
+              <>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </button>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate("/register")}
+                >
+                  Register
+                </button>
+              </>
+            )}
           </div>
         </div>
       </nav>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "../pages/Main";
-import Login from "../pages/Main";
-import Register from "../pages/Register";
-import MovieDetail from "../pages/MovieDetail";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/details/:id" element={<MovieDetail />}></Route>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
   );
