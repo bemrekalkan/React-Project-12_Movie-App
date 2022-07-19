edebb52fe07333b2f365047c73b2b3aeb9649369
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-19 20:36:41 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <p>Navbar</p>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand text-white">
+            <h4>React Movie App</h4>
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "../pages/Main";
 import Login from "../pages/Main";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
+import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/details/:id" element={<MovieDetail />}></Route>
       </Routes>
     </BrowserRouter>
   );
