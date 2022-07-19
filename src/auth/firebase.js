22e724b5734a29f85d464dc53b2e9659bd622aef
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-19 22:21:10 +0300

    firebase

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 //* https://console.firebase.google.com/ => project settings
 //! get firebaseconfig settings from firebase console settings
 const firebaseConfig = {
-  apiKey: "AIzaSyDReWYi7eBjaJ2d3xQI5gnNGkMuxNuKSjo",
-  authDomain: "movie-application-21f1b.firebaseapp.com",
-  projectId: "movie-application-21f1b",
-  storageBucket: "movie-application-21f1b.appspot.com",
-  messagingSenderId: "981308327583",
-  appId: "1:981308327583:web:704fb96cd8bcb7494604aa",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
