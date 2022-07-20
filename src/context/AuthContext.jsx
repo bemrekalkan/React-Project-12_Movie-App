24a1d4164bf7614ca7268f352ccd3f9e94a9a687
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-20 20:14:37 +0300

    .

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useState } from 'react';
-import { createContext } from 'react';
+import React from "react";
+import { useState } from "react";
+import { createContext } from "react";
 
 export const AuthContext = createContext();
 
@@ -8,7 +8,7 @@ const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrenUser] = useState(true);
 
   return (
-    <AuthContext.Provider value={{ currentUser }}>
+    <AuthContext.Provider value={{ currentUser, first }}>
       {children}
     </AuthContext.Provider>
   );
