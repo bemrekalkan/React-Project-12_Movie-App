f3429d871a6fd34293e6fff604bad604f7789124
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-20 21:56:49 +0300

    Main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,34 +1,35 @@
 import axios from "axios";
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
   useEffect(() => {
     getMovies(FEATURED_API);
-  });
+  }, []);
+
   const getMovies = (API) => {
     setLoading(true);
     axios
       .get(API)
-      .then((res) => console.log(res.data.results))
+      .then((res) => setMovies(res.data.results))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   };
   return (
-    <div className="d-flex justify-content-center flex-wrap">
+    <div className="d-flex justify-content-center flex-wrap ">
       {loading ? (
         <div className="spinner-border text-primary m-5" role="status">
           <span className="sr-only">Loading...</span>
         </div>
       ) : (
-        movies?.map((movie) => null)
+        movies?.map((movie) => <MovieCard key={movie.id} {...movie} />)
       )}
     </div>
   );
