8ddd23f910b7e33fb5bf38aac84bfeaf5cf453ef
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-21 08:54:48 +0300

    VideoSection Added.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,17 @@
+import React, { useContext, useState } from "react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useContext } from "react";
+import { useEffect } from "react";
 import MovieCard from "../components/MovieCard";
 import { AuthContext } from "../context/AuthContext";
+import { toastWarnNotify } from "../helpers/ToastNotify";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
-  const [movies, setMovies] = React.useState([]);
-  const [loading, setLoading] = React.useState(false);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const { currentUser } = useContext(AuthContext);
 
@@ -32,12 +33,13 @@ const Main = () => {
     if (searchTerm && currentUser) {
       getMovies(SEARCH_API + searchTerm);
     } else if (!currentUser) {
-      alert("Please log in to search a movie");
+      toastWarnNotify("Please log in to search a movie");
+      // alert("Please log in to search a movie");
     } else {
-      alert("Please enter a text");
+      toastWarnNotify("Please enter a text");
+      // alert("Please enter a text");
     }
   };
-
   return (
     <>
       <form className="search" onSubmit={handleSubmit}>
@@ -49,7 +51,7 @@ const Main = () => {
         />
         <button type="submit">Search</button>
       </form>
-      <div className="d-flex justify-content-center flex-wrap ">
+      <div className="d-flex justify-content-center flex-wrap">
         {loading ? (
           <div className="spinner-border text-primary m-5" role="status">
             <span className="sr-only">Loading...</span>
